9678c9c0fc65723b7a38b7d7b981eb5d0932739d
Vicky Law <vicky.law1217@gmail.com>
2019-09-17 11:00:10 -0400

    set import routes in server js

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,15 @@ const connection = require("./connection.js");
 
 let orm = {
 
-    selectAll: function()
+    selectAll: function (tableInput, callback) {
+        let queryString = "SELECT * FROM " + tableInput
+            + ";"; connection.query(queryString, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                callback(result)
+            });
+    },
 
 
 
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const exphbs = require("express-handlebars");
 const express = require("express");
-const mysql = require("mysql");
+//const mysql = require("mysql");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-app.use(express.urlenconded({ extended: true }));
-app.use(express.joson());
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them.
+let routes = require("./controllers/burgers_controllers.js");
+app.use(routes);
+
 app.listen(PORT, function () {
-    console.log("Server Listening on: http://localhost" + PORT);
+    console.log("Server Listening on: http://localhost " + PORT);
 })
