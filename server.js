e70b4f13168583a66b5bd5ae022437522fb43ecf
Vicky Law <vicky.law1217@gmail.com>
2019-09-17 11:20:41 -0400

    correct to setup connection and server

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
-const exphbs = require("express-handlebars");
-const express = require("express");
-//const mysql = require("mysql");
+var express = require("express");
 
-const app = express();
+var PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 8080;
+var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse request body as JSON
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-let routes = require("./controllers/burgers_controllers.js");
-app.use(routes);
+/*var routes = require("./controllers/burgers_controllers.js");
 
+app.use(routes);*/
+
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
-    console.log("Server Listening on: http://localhost " + PORT);
-})
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
